0258be4f4b4c10120076675ccb26d67358bea6d2
tomy <tomyrios2006@gmail.com>
2025-07-31 20:55:18 -0300

    hotfix/puppeteer

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,31 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Custom webpack configuration to avoid bundling of dynamic-require packages that break the build
+  experimental: {
+    serverComponentsExternalPackages: [
+      "puppeteer",
+      "puppeteer-extra",
+      "puppeteer-extra-plugin-stealth",
+    ],
+  },
+
+  outputFileTracingIncludes: {
+    "app/api/**": [
+      "./node_modules/puppeteer/**",
+      "./node_modules/puppeteer-extra/**",
+      "./node_modules/puppeteer-extra-plugin-stealth/**",
+    ],
+  },
+
   webpack: (config, { isServer }) => {
-    // Only adjust the server bundle since the libraries are never executed in the browser
     if (isServer) {
-      const externalsToExclude = [
+      config.externals = [
+        ...(Array.isArray(config.externals) ? config.externals : []),
         "puppeteer",
         "puppeteer-extra",
         "puppeteer-extra-plugin-stealth",
-        "clone-deep",
-        "merge-deep",
-        "shallow-clone",
-        "is-plain-object",
-        "kind-of",
-        "for-own",
-        "lazy-cache",
       ];
-
-      // If externals is an array we can simply push, otherwise preserve the original value
-      if (Array.isArray(config.externals)) {
-        config.externals.push(...externalsToExclude);
-      }
     }
-
     return config;
   },
 };
